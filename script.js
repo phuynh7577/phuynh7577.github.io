6fce0adec7c7c59cb2e8476dfc8d60d949321074
Phi Huynh <huynh@Phis-MBP.fios-router.home>
2020-01-30 23:05:11 -0500

    added some css and html

---
+++ script.js
@@ -0,0 +1,31 @@
+const houseType = ["Single Family", "Condo", "Multi-Family", "Land", "Rentals", "Pending"]
+const beds = [1, 2, 3, 4, 5, 6]
+const baths = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5]
+const minPrice = [25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 250000, 300000, 350000, 400000, 450000, 500000]
+const maxPrice = [25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 250000, 300000, 350000, 400000, 450000, 500000]
+
+
+
+
+
+$ (() => {
+
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://realtor.p.rapidapi.com/properties/detail?listing_id=608763437&prop_status=for_sale&property_id=4599450556",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "realtor.p.rapidapi.com",
+// 		"x-rapidapi-key": "a7e18599a7msh0e4b44020cda7e2p156c1ejsnd934c76f8c56"
+// 	}
+// }
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+
+
+
+})
