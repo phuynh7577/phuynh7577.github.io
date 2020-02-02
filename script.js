c6fc5cecaed6d3be62b1ff4e3ddc8ab9ef71e9bc
Phi Huynh <huynh@Phis-MBP.fios-router.home>
2020-02-01 21:32:48 -0500

    added houses and multifamily to modal and updated some ccs

--- script.js
+++ script.js
@@ -1,34 +1,154 @@
-const houseType = ["Single Family", "Condo", "Multi-Family", "Land", "Rentals", "Pending"]
-const beds = [1, 2, 3, 4, 5, 6]
-const baths = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5]
-const minPrice = [25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 250000, 300000, 350000, 400000, 450000, 500000]
-const maxPrice = [25000, 50000, 75000, 100000, 125000, 150000, 175000, 200000, 250000, 300000, 350000, 400000, 450000, 500000]
-
-
-
 
 
 $ (() => {
 
+  //house modal
     const $openBtn = $('#open-modal-houses');
     const $modal = $('#modal-house');
     const $closeBtn = $('#close');
     
-    // Event handler to open the modal
-    const openModal = () => {
-      $modal.css('display', 'block');
+      // Event handler to open the modal
+      const openModal = () => {
+        $modal.css('display', 'block');
+      }
+      
+      // Event handler to close the modal
+      const closeModal = () => {
+        $modal.css('display', 'none');
+      }
+      
+      //adding event listener
+      $openBtn.on('click', openModal);
+      
+      //Add event listener to Close button
+      $closeBtn.on('click', closeModal);
+
+
+
+            //multifamily modal
+            const $openBtnMulti = $('#open-modal-multifamily');
+            const $modalMulti = $('#modal-multifamily');
+            const $closeBtnMulti = $('#close-multi');
+
+            // Event handler to open the modal
+            const openModalMulti = () => {
+              $modalMulti.css('display', 'block');
+            }
+
+            // Event handler to close the modal
+            const closeModalMulti = () => {
+              $modalMulti.css('display', 'none');
+            }
+
+            //adding event listener
+            $openBtnMulti.on('click', openModalMulti);
+
+            //Add event listener to Close button
+            $closeBtnMulti.on('click', closeModalMulti);
+
+
+
+
+
+  var house = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://realtor.p.rapidapi.com/properties/list-for-sale?is_pending=false&sort=relevance&radius=10&prop_type=single_family&city=Providence&offset=0&limit=400&state_code=RI",
+    "method": "GET",
+    "headers": {
+          "x-rapidapi-host": "realtor.p.rapidapi.com",
+          "x-rapidapi-key": "a7e18599a7msh0e4b44020cda7e2p156c1ejsnd934c76f8c56"
+        }
+      }
+            
+
+      $.ajax(house).then( (data) => {
+        // console.log(data);
+        
+        for (let i = 0; i < data.listings.length; i++) {
+          
+          const div1 = $("<div>").addClass("single-div")
+          $("#modal-divs").append(div1)
+          
+          const h1 = $("<h1>")
+
+          const h2 = $("<h2>")
+
+          const span = $("<span>")
+          
+          const spanTwo = $("<span>")
+
+          $(div1).append(h1)
+          $(div1).append(h2)
+          $(div1).append(span)
+          $(div1).append(spanTwo)
+          
+
+          h1.text(`${data.listings[i].address}`)
+          
+          h2.text(`${data.listings[i].price}`)
+
+          const image = $("<img>").attr("src", data.listings[i].photo).addClass("img")
+          $(div1).append(image)
+          
+          span.text(`SIZE: ${data.listings[i].sqft_raw}sqft`)
+  
+          spanTwo.text(`Rooms: ${data.listings[i].beds}Beds ${data.listings[i].baths}Baths`)
+
+        }
+      })
+
+// multi family modal
+    var multi = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://realtor.p.rapidapi.com/properties/list-for-sale?sort=relevance&radius=10&prop_type=multi_family&city=Providence&offset=0&limit=400&state_code=RI",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "realtor.p.rapidapi.com",
+        "x-rapidapi-key": "a7e18599a7msh0e4b44020cda7e2p156c1ejsnd934c76f8c56"
+      }
     }
+        
+        $.ajax(multi).then( (data) => {
+          console.log(multi);
+
+          for (let i = 0; i < data.listings.length; i++) {
+          
+            const div1 = $("<div>").addClass("single-div")
+            $("#divs-multifamily").append(div1)
+            
+            const h1 = $("<h1>")
+  
+            const h2 = $("<h2>")
+  
+            const span = $("<span>")
+            
+            const spanTwo = $("<span>")
+  
+            $(div1).append(h1)
+            $(div1).append(h2)
+            $(div1).append(span)
+            $(div1).append(spanTwo)
+            
+  
+            h1.text(`${data.listings[i].address}`)
+            
+            h2.text(`${data.listings[i].price}`)
+  
+            const image = $("<img>").attr("src", data.listings[i].photo).addClass("img")
+            $(div1).append(image)
+            
+            span.text(`SIZE: ${data.listings[i].sqft_raw}sqft`)
     
-    // Event handler to close the modal
-    const closeModal = () => {
-      $modal.css('display', 'none');
-    }
-    
-    //adding event listener
-    $openBtn.on('click', openModal);
-    
-    //Add event listener to Close button
-    $closeBtn.on('click', closeModal);
+            spanTwo.text(`Rooms: ${data.listings[i].beds}Beds ${data.listings[i].baths}Baths`)
+
+          }
+        })
+      
+
+
+
 
 
 })
